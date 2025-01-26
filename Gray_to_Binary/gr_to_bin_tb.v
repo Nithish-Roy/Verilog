8ce7689216c418176244ec8e7165c5ef0101d414
Nithish Roy <roynithish70@gmail.com>
2025-01-26 21:27:08 +0530

    Create gr_to_bin_tb.v

---
+++ Gray_to_Binary/gr_to_bin_tb.v
@@ -0,0 +1,13 @@
+module gtob_tb #(parameter N = 4)();
+reg [N-1:0] data_in;
+wire [N-1:0]data_out;
+gtob inst1(.data_in(data_in),.data_out(data_out));
+initial 
+begin
+$monitor(" data_in = %b data_out = %b",data_in,data_out);
+#5 data_in = $random ; 
+#5 data_in = $random ; 
+#5 data_in = $random ; 
+#5 data_in = $random ; 
+end
+endmodule
