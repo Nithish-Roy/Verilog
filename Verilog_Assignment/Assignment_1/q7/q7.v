ab8b34314126089cff4816f77136d7298bee7839
Nithish Roy <roynithish70@gmail.com>
2025-01-29 21:52:51 +0530

    Update q7.v

--- Verilog_Assignment/Assignment_1/q7/q7.v
+++ Verilog_Assignment/Assignment_1/q7/q7.v
@@ -1,3 +1,5 @@
+//Write Verilog code for generating an infinite clock signal with a 50%
+//Duty cycle having a time period of 10ns using a forever loop.
 `timescale 1ns / 1ps
 module clk_gen(clk);
 reg clk = 0;
