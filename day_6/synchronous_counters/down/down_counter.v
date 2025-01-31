66e16ea0dd0749e5a1c9eab9c2b679332c2af7c3
Nithish Roy <roynithish70@gmail.com>
2025-01-31 22:15:44 +0530

    Create down_counter.v

---
+++ day_6/synchronous_counters/down/down_counter.v
@@ -0,0 +1,10 @@
+module up_count_syn #(parameter QBIT = 10)(input clk,
+		    input rst,
+	    	    output reg [QBIT-1:0] q);
+	    always@(posedge clk)begin
+		    if(rst)
+			    q <= 0;
+		    else
+			    q = q - 1'b1;
+	    end
+	    endmodule
