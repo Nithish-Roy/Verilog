ec6f8d48e793cd25b4d493cf7f266bde6dc0e19b
Nithish Roy <roynithish70@gmail.com>
2025-02-01 22:26:09 +0530

    Update SD_010.v

--- day_7/MOORE/SD_010.v
+++ day_7/MOORE/SD_010.v
@@ -1,44 +1,42 @@
-module seqdc();
-	reg clk;
-	reg rst;
-	reg q_in;
-	wire q;
+module sd(
+		input clk,
+		input rst,
+		input q_in,
+		output q);
+
+	parameter [2:0] A = 2'B00,
+			B = 2'B01,
+			C = 2'B10,
+			D = 2'B11;
 	
-	sd inst1(clk , rst , q_in , q);
+	reg [2:0] present;
+	reg [2:0] next;
 
-	initial begin 
-		$dumpfile("dump.vcd");
-		$dumpvars;
-		$monitor(" q_in = %b q = %b",q_in , q);
-	end
+	always @(posedge clk)
+		if(!rst)
+			present <= A;
+		else
+			present <= next;
 
-		initial begin
-			rst = 1;	
-			clk = 0;
-			forever #5 clk = ~clk;
+	always @(present , q_in)
+		begin
+			
+		case (present)
+				A: 
+					if(!q_in) next = B;
+					else 	 next = A;
+				B:
+					if(q_in) next = C;
+					else 	next = B;
+				C:
+					if(!q_in) next = D;
+					else 	 next = A;
+				D:	
+					if(!q_in) next = A;
+					//next = A; //for non_overlaapping
+					else next = 	A;
+
+			endcase
 		end
-	initial begin
-      @(posedge clk) q_in <= 1;
-		#5 rst = 0;
-		#10 rst = 1;
-      @(posedge clk) q_in <= 1;
-      @(posedge clk) q_in <= 0;
-      @(posedge clk) q_in <= 1;
-      @(posedge clk) q_in <= 1; 
-      @(posedge clk) q_in <= 0;
-      @(posedge clk) q_in <= 0;
-      @(posedge clk) q_in <= 1;
-      @(posedge clk) q_in <= 1;
-      @(posedge clk) q_in <= 0;
-      @(posedge clk) q_in <= 1;
-      @(posedge clk) q_in <= 0; 
-      @(posedge clk) q_in <= 1;
-      @(posedge clk) q_in <= 0;
-      @(posedge clk) q_in <= 0; 
-
-	end
-		initial begin 
-			#200;
-			$finish;
-		end
-endmodule	
+	assign q = (present == D);
+endmodule
