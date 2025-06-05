e1adbd076cba198835325d6901727c8b2a409656
Nithish Roy <roynithish70@gmail.com>
2025-06-05 20:08:47 +0530

    Create diamond.v

---
+++ pattern/diamond.v
@@ -0,0 +1,25 @@
+module pattern_triangle;
+int i, j, k;
+  
+  initial begin
+    for(i = 0; i <= 5; i = i+1)
+      begin
+      for(j = 1; j <= i ; j =j+1)
+        $write("%0d", i);
+      $display("");
+      end
+    
+    i = 0; j = 0; k = 4;
+    
+    for(i = 1; i <= 5; i = i+1)
+      begin
+        for(j = 5; j > i ; j =j-1)
+          begin
+            $write("%0d", k);
+          end
+        k = k - 1;
+        $display("");
+      end
+    
+  end
+endmodule
